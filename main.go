b1dd4e5616814ca7800a1d8e79f8a3dd7a5bec60
imelda <imeldamayantisipit@gmail.com>
2026-07-05 20:57:45 +0800

    Change path v3

--- main.go
+++ main.go
@@ -2,40 +2,33 @@ package main
 
 import (
 	"KnapSack/config"
+	"KnapSack/assets"
 	"KnapSack/controllers/aboutcontroller"
 	"KnapSack/controllers/homecontroller"
 	"KnapSack/controllers/makanancontroller"
-
 	// "KnapSack/models/makananmodel"
 	"log"
 	"net/http"
+	"os"
 )
-
-// var tmpl *template.Template
-
-// func init(){
-// 	tmpl = template.Must(template.ParseGlob("views/home/*.html"))
-// }
-
-// func homeHandler(w http.ResponseWriter, r *http.Request) {
-// 	tmpl.ExecuteTemplate(w, "index.html", nil)
-// }
 
 func main() {
 	config.ConnectDB()
-	// makananmodel.GetAll()
 
-	fs := http.FileServer(http.Dir("assets"))
+	fs := http.FileServer(http.FS(assets.FS))
 	http.Handle("/assets/", http.StripPrefix("/assets", fs))
-	// http.HandleFunc("/", homeHandler)
 
 	http.HandleFunc("/", homecontroller.Splash)
-	// http.HandleFunc("/generate", homecontroller.Generate)
 	http.HandleFunc("/generated-data", homecontroller.ShowGenerated)
 	http.HandleFunc("/data", makanancontroller.Index)
 	http.HandleFunc("/about", aboutcontroller.Index)
 
-	log.Println("Server running on port 3001")
-	http.ListenAndServe("127.0.0.1:3001", nil)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3001"
+	}
+
+	log.Println("Server running on port " + port)
+	http.ListenAndServe("0.0.0.0:"+port, nil)
 }
 
